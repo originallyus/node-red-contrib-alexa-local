932cea701e1bf938fdf60750d1b1bf19242758c5
Torin Nguyen <torinnguyen@gmail.com>
2017-10-13 00:08:03 +0700

    Detect increase/decrease command

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ module.exports = function(RED)
     //variables placed here are shared by all nodes
     var storage = require('node-persist');
 
+    //this should be a number that does not round nicely when convert to 0-100 range
+    const bri_default = 126;
+
     //NodeRED node constructor
     function AlexaLocalNode(config) 
     {
@@ -123,7 +126,7 @@ module.exports = function(RED)
 
     function constructOneLightConfig(lightId, deviceName, httpPort)
     {
-        return '{"state": {"on": true, "bri": 128, "hue": 15823, "sat": 88, "effect": "none", "ct": 313, "alert": "none", "colormode": "ct", "ct": 365, "reachable": true, "xy": [0.4255, 0.3998]}, "type": "Extended color light", "name": "' + deviceName + '", "modelid": "LCT004", "manufacturername": "Philips", "uniqueid": "' + lightId + '", "swversion": "65003148", "pointsymbol": {"1": "none", "2": "none", "3": "none", "4": "none", "5": "none", "6": "none", "7": "none", "8": "none"}}';
+        return '{"state": {"on": true, "bri": ' + bri_default + ', "hue": 15823, "sat": 88, "effect": "none", "ct": 313, "alert": "none", "colormode": "ct", "ct": 365, "reachable": true, "xy": [0.4255, 0.3998]}, "type": "Extended color light", "name": "' + deviceName + '", "modelid": "LCT004", "manufacturername": "Philips", "uniqueid": "' + lightId + '", "swversion": "65003148", "pointsymbol": {"1": "none", "2": "none", "3": "none", "4": "none", "5": "none", "6": "none", "7": "none", "8": "none"}}';
     }
 
     function constructBridgeSetupXml(lightId, deviceName, httpPort)
@@ -262,6 +265,13 @@ module.exports = function(RED)
         if (request.data.on)
             onoff = "on";
         msg.payload = onoff;
+
+        //Detect increase/decrease command
+        msg.change_direction = 0;
+        if (msg.bri && msg.bri == bri_default - 64)     //magic number
+            msg.change_direction = -1;
+        if (msg.bri && msg.bri == bri_default + 63)     //magic number
+            msg.change_direction = 1;
 
         //Massage brightness parameter
         if (msg.bri) {
